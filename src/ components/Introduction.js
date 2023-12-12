b7d7412d906e6b1bb7409de9887b4d31239af418
Kaua Gabriell <kg3guarino@gmail.com>
2023-12-12 15:17:26 -0300

    Introdução pronta

--- src/ components/Introduction.js
+++ src/ components/Introduction.js
@@ -1,11 +1,45 @@
-import gif from '/home/kauag/github/portifolio/src/imagens/Developer activity.gif';
+import arrow from '/home/kauag/github/Portifolio/src/imagens/arrow-down.svg';
+import linkedin from '/home/kauag/github/Portifolio/src/imagens/linkedin.svg';
+import github from '/home/kauag/github/Portifolio/src/imagens/github.svg';
+import gmail from '/home/kauag/github/Portifolio/src/imagens/envelope-at.svg';
 export default function Introduction() {
   return (
-    <main className="container">
-      <h1 style={{ fontSize: '5rem', maxWidth: '13ch' }}>
-        Olá, Eu sou <span className="mark-main">Dev Front-End</span>
-      </h1>
-      <img src={gif} alt="gif programmer" className="img-section" />
-    </main>
+    <section className="container center section-introducao">
+      <div
+        className="container center"
+        style={{ flexGrow: '1', width: '100%' }}
+      >
+        <h1
+          style={{ color: '#cc005f', fontSize: '2rem', lineHeight: '1.5rem' }}
+        >
+          Olá!
+        </h1>
+        <h2 style={{ fontSize: '5rem', lineHeight: '110%' }}>Eu sou Kauã</h2>
+        <h3 style={{ fontSize: '1.5rem', color: '#00A97F' }}>
+          Front End Developer
+        </h3>
+        <a href="#" className="button-about-me">
+          Sobre mim{' '}
+          <span>
+            <img src={arrow} alt="arrow down" />
+          </span>
+        </a>
+      </div>
+      <footer className="social-midias">
+        <a href="https://www.linkedin.com/in/kauagabriellkgg/" target="blank">
+          <img
+            src={linkedin}
+            alt="Linkedin Icon"
+            className="icon-social-midia"
+          />
+        </a>
+        <a href="https://github.com/Kauaguarinogabriell" target="blank">
+          <img src={github} alt="Github Icon" className="icon-social-midia" />
+        </a>
+        <a href="mailto:kg3guarino@gmail.com" target="blank">
+          <img src={gmail} alt="Gmail Icon" className="icon-social-midia" />
+        </a>
+      </footer>
+    </section>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import About from './ components/About.js';
-import Header from './ components/Header.js';
-import Introduction from './ components/Introduction.js';
-import Habilidades from './ components/Habilidades.js';
-import Projetos from './ components/Projetos.js';
+import Introduction from './ components/Introduction';
 import style from './style.css';
 function App() {
   return (
     <div className="App">
-      <Header />
       <Introduction />
-      <About />
-      <Habilidades />
-      <Projetos />
     </div>
   );
 }
